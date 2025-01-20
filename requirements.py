2e9e12ec8b4438845516a58f6a083c69fafc8183
laiyoi <zz15091429981@163.com>
2025-01-20 18:05:47 +0800

    update

--- requirements.py
+++ requirements.py
@@ -7,7 +7,7 @@ plugins = nonebot_data.get("plugins", [])
 #创建tqdm进度条
 pbar = tqdm.tqdm(total=len(plugins))
 for plugin in plugins:
-    command = f"pip install {plugin} -U --force -i https://pypi.tuna.tsinghua.edu.cn/simple"
+    command = f"pip install {plugin} -U -i https://pypi.tuna.tsinghua.edu.cn/simple " #--force"
     #os.system(f'powershell /c "conda activate nb"')
     os.system(command)
 
